69f0e4622b80ad8b201f9adb03ffa710e8b25265
Thomas A. Rieck <tom.rieck@radiantsolutions.com>
2020-12-17 11:30:59 -0500

    Convert to tensors for performance.

--- src/Tensor.cu
+++ src/Tensor.cu
@@ -16,6 +16,8 @@
 
 #include "Error.h"
 #include "Tensor.h"
+#include <boost/chrono.hpp>
+#include <boost/thread/thread.hpp>
 #include <thrust/device_vector.h>
 #include <thrust/random.h>
 #include <xtensor/xarray.hpp>
@@ -53,9 +55,9 @@ struct random_functor
 };
 
 template<typename T, typename I, std::size_t N>
-auto cuda_random(const I (& shape)[N], T lower = 0, T upper = 1)
+inline auto cuda_random(const I (& shape)[N], T lower = 0, T upper = 1) noexcept
 {
-    auto output = cuda_array<T>::from_shape(shape);
+    auto output = cuda_tensor<T, N>::from_shape(shape);
 
     thrust::counting_iterator<int> sequence(0);
     thrust::transform(sequence, sequence + output.size(), output.begin(), random_functor<T>(lower, upper));
@@ -63,17 +65,80 @@ auto cuda_random(const I (& shape)[N], T lower = 0, T upper = 1)
     return output;
 }
 
+template<class T, class I, std::size_t N>
+inline auto cuda_fill(const I (& shape)[N], T value) noexcept
+{
+    auto output = cuda_tensor<T, N>::from_shape(shape);
+
+    thrust::fill(std::begin(output), std::end(output), value);
+
+    return output;
+}
+
+template<class T, class I, std::size_t N>
+inline auto cuda_ones(const I (& shape)[N]) noexcept
+{
+    return cuda_fill<T>(shape, 1);
+}
+
+template<class T, class I, std::size_t N>
+inline auto cuda_empty(const I (& shape)[N]) noexcept
+{
+    return cuda_tensor<T, N>::from_shape(shape);
+}
+
+template<typename T, typename I, std::size_t N>
+void test_random(const I (& shape)[N], T lower = 0, T upper = 1)
+{
+    const auto r = cuda_random<T>(shape, lower, upper);
+
+    int i = 0;
+    for (const auto& value: r) {
+        std::cout << ++i << "    " << value << std::endl;
+    }
+}
+
+template<typename T, typename I, std::size_t N>
+void test_fill(const I (& shape)[N], T value)
+{
+    const auto r = cuda_fill<T>(shape, value);
+
+    int i = 0;
+    for (const auto& v: r) {
+        std::cout << ++i << "    " << v << std::endl;
+    }
+}
+
+template<typename T, typename I, std::size_t N>
+void test_empty(const I (& shape)[N])
+{
+    cuda_tensor<T, N> r = cuda_empty<T>(shape);
+
+    int i = 0;
+    for (const auto& v: r) {
+        std::cout << ++i << "    " << v << std::endl;
+    }
+}
+
+void sleep(int seconds)
+{
+    boost::this_thread::sleep_for(boost::chrono::seconds(seconds));
+}
+
 class TensorImpl
 {
 public:
     TensorImpl(Device device) : dev_(device)
     {
-        auto r = cuda_random<float>({1000, 1000, 10});
+        test_random<float>({10, 10, 10}, 0, 100);
 
-        int i = 0;
-        for (const auto& value: r) {
-            std::cout << ++i << "    " << value << std::endl;
-        }
+        sleep(1);
+
+        test_fill<double>({10, 10, 10}, 18.0123456789);
+
+        sleep(1);
+
+        test_empty<int>({10, 10, 10});
     }
 
     ~TensorImpl()
